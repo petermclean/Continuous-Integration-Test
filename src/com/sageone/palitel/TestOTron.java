ffae0a2c35cf150532baad60fdd38266fc472a77
Peter McLean <peter.mclean@sage.com>
2015-07-17 15:22:23 +0100

    Update TestOTron.java

--- src/com/sageone/palitel/TestOTron.java
+++ src/com/sageone/palitel/TestOTron.java
@@ -15,7 +15,8 @@ public class TestOTron {
     public void setUp(){
         client = new Client(host, port, true);
         client.setProjectBaseDirectory(projectBaseDirectory);
-        client.setReporter("xml", "reports", "TestOTron");
+        //client.setReporter("xml", "reports", "TestOTron");
+        client.setReporter("xml", "C:/reports", "TestOTron");
     }
 
     @Test
