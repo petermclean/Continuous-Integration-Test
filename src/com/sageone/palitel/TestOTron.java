065d0aef140a4ac6a3578cb1a0cf3926540d1aec
Peter McLean <peter.mclean@sage.com>
2015-07-19 21:31:23 +0100

    Update TestOTron.java

--- src/com/sageone/palitel/TestOTron.java
+++ src/com/sageone/palitel/TestOTron.java
@@ -5,7 +5,7 @@ import org.junit.*;
  *
 */
 public class TestOTron {
-    private String host = "192.168.0.10";
+    private String host = "77.103.58.53";
     private int port = 8889;
     private String projectBaseDirectory = "C:/Jenkins/palitel";
     //private String projectBaseDirectory = "/Users/Shared/Jenkins/palitel";
